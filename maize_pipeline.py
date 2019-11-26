ebb6c3e7e16b7cf59b458de897c873c05007c507
beleafs <43150328+beleafs1276@users.noreply.github.com>
2019-11-25 22:56:36 -0600

    Update maize_pipeline.py

--- maize_pipeline.py
+++ maize_pipeline.py
@@ -29,23 +29,32 @@ output = args.output
 
 print("Building GMAP database...")
 subprocess.run(["gmap_build", "-d", f"database/Genome{target}", f"database/{target}.chromosomes.fasta"])
+subprocess.run(["gmap_build", "-d", f"database/Genome{source}", f"database/{source}.chromosomes.fasta"])
 
 print("Mapping with GMAP...")
 command = f"gmap -d Genome{target} database/{target}.transcripts.fasta -D database/ -f 2 > database/{source}to{target}_match_gene.gff3"
 os.system(command)
-
-print("Generating tracking file using gffcompare...")
-subprocess.run(["gffcompare", f"database/{source}to{target}_match_gene.gff3", "-r", f"database/{target}.gff3", "-o", f"result_{source}_to_{target}", "-T"])
-
-print("Running blastn...")
-command = f"blastn -subject {target}.transcripts.fasta> -query {source}.transcripts.fasta -outfmt 6 > blastn_{source}_to_{target}.dms"
+command = f"gmap -d Genome{source} database/{source}.transcripts.fasta -D database/ -f 2 > database/{target}to{source}_match_gene.gff3"
 os.system(command)
 
-#print("Read and processing blastn results...")
-#dbA = gffutils.FeatureDB("database/chromosomeA")
-#dbC = gffutils.FeatureDB("database/chromosomeC")
-#read_blastn("database/blastn_A_to_C", dbA, dbC)
-#read_blastn("database/blastn_C_to_A", dbC, dbA)
+print("Generating tracking file using gffcompare...")
+subprocess.run(["gffcompare", f"database/{source}to{target}_match_gene.gff3", "-r", f"database/{target}.gff3", "-o", f"database/{source}to{target}GMAP", "-T"])
+subprocess.run(["gffcompare", f"database/{target}to{source}_match_gene.gff3", "-r", f"database/{source}.gff3", "-o", f"database/{target}to{source}GMAP", "-T"])
+
+print("Running blastn...")
+command = f"blastn -subject database/{target}.transcripts.fasta> -query database/{source}.transcripts.fasta -outfmt 6 > database/blastn_{source}_to_{target}"
+os.system(command)
+command = f"blastn -subject database/{source}.transcripts.fasta> -query database/{target}.transcripts.fasta -outfmt 6 > database/blastn_{target}_to_{source}"
+os.system(command)
+
+# creating database
+dbA = gffutils.create_db(f"database/{source}.gff3", f"database/chromosome{source}")
+dbB = gffutils.create_db(f"database/{target}.gff3", f"database/chromosome{target}")
+
+print("Read and processing blastn results...")
+read_blastn(f"database/blastn_{source}_to_{target}", dbA, dbB)
+read_blastn(f"database/blastn_{target}_to_{source}", dbB, dbA)
 
 print("Running trmap...")
-subprocess.run(["trmap", "-o", f"database/{source}to{target}GMAP.gff3", f"database/{target}.gff3", f"{source}to{target}map_match_gene.gff3"])
+subprocess.run(["trmap", "-o", f"database/{source}to{target}GMAP.gff3", f"database/{target}.gff3", f"database/{source}to{target}_match_gene.gff3"])
+subprocess.run(["trmap", "-o", f"database/{target}to{source}GMAP.gff3", f"database/{source}.gff3", f"database/{target}to{source}_match_gene.gff3"])
